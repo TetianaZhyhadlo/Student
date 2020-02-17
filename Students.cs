168a03faf60b4e0c22edfb3922d39895eb3e8998
Татьяна Жигадло <t.zhyhadlo@gmail.com>
2020-02-17 19:30:11 +0200

    Add project files.

---
+++ Students.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Student
+{
+    class Students
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Course { get; set; }
+        public int Level { get; set; }
+    }
+}
---
+++ Teacher.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Student
+{
+    public class Teacher
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Subject { get; set; }
+        public string Shcool { get; set; }
+    }
+}
